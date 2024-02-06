3d4a39c6798062c37d942c9468e4ce6345050380
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-06 15:55:08 -0500

    Basic introduction in JavaScript

---
+++ 1-Introduction/index.js
@@ -0,0 +1,10 @@
+//console.log(`Hello Yashar`);
+//console.log(`I love food`);
+
+//window.alert(`Warning`);
+//alert("hi");
+
+// This is a comment
+
+document.getElementById("myH1").textContent = "Yashar";
+document.getElementById("myP1").textContent = `Tukhfatullin`;
