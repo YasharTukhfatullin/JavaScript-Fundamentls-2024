de00ebe71d5cc2d68737df47160f3e4f95e0ba8a
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-07 12:01:59 -0500

    Tested math objects

---
+++ 8-MathObjects/index.js
@@ -0,0 +1,50 @@
+/**
+ * Math = built-in object that provides a collection of properties and methods.
+ */
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log("============")
+
+let x = 3.99;
+let y = 2;
+let z;
+
+z = Math.round(x);
+console.log(z)
+
+z = Math.floor(x);
+console.log(z)
+
+z = Math.ceil(z);
+console.log(z)
+
+z = Math.trunc(x);
+console.log(z)
+
+z = Math.pow(x, y);
+console.log(z)
+
+z = Math.sqrt(x);
+console.log(z)
+
+z = Math.log(x);
+console.log(z)
+
+z = Math.sin(x);
+console.log(z)
+
+z = Math.cos(x);
+console.log(z)
+
+z = Math.tan(x);
+console.log(z)
+
+z = Math.abs(x);
+console.log(z)
+
+z = Math.sign(x);
+console.log(z)
+
+
+
