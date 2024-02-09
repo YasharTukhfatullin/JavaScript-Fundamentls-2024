4153315e6523c72a37049079ca2c200cfcbeae4a
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-09 16:24:20 -0500

    Calculating Celsius to Fahrenheit and vice versa.

--- 24-TemperatureConversionProgram/index.js
+++ 24-TemperatureConversionProgram/index.js
@@ -2,6 +2,23 @@
  * Temperature Conversion Program
  */
 
-function convert() {
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
 
+let temperature;
+
+function convert() {
+  if (toFahrenheit.checked) {
+    temperature = Number(textBox.value);
+    temperature = temperature * 9 / 5 + 32;
+    result.textContent = temperature.toFixed(1) + "°F";
+  } else if (toCelsius.checked) {
+    temperature = Number(textBox.value);
+    temperature = (temperature - 32) * (5/9);
+    result.textContent = temperature.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Select a unit"
+  }
 };
