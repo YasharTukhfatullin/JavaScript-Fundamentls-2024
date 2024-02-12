f0f5bf7d8f0f84ff914cab119ae37cf340119c52
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-12 16:14:43 -0500

    Added extra comment

--- 32-Map()/index.js
+++ 32-Map()/index.js
@@ -1,6 +1,8 @@
 /**
  * .map() = accept a callback and applies that function 
  *          to each element of an array, then return a new array.
+ * 
+ *          It preserves original array but forEach doesn't do that it changes it 
  */
 
 const numbers = [1, 2, 3, 4, 5];
